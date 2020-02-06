001b7e7ceafd4a8f4282830873c0dd7952028b91
Code-Chingy <chingy.debillz@gmail.con>
2020-02-06 01:31:02 +0000

    initial commit

---
+++ Chingy++ Compiler/include/Test.h
@@ -0,0 +1,22 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+
+class Test
+{
+    public:
+        Test();
+        virtual ~Test();
+
+    protected:
+
+    private:
+};
+
+#endif // TEST_H
---
+++ Chingy++ Compiler/src/Test.cpp
@@ -0,0 +1,16 @@
+#include "Test.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+Test::Test()
+{
+    //ctor
+    cout << MONEY;
+}
+
+Test::~Test()
+{
+    //dtor
+}
